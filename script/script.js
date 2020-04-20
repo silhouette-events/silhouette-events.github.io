4ea3924d9fcfed051cb424300c060c1fc3c432b7
Isaiah Meek <50851883+Isaiah-DSW@users.noreply.github.com>
2020-04-20 19:18:10 +1200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -33,3 +33,45 @@ player.addEventListener(Twitch.Player.OFFLINE, function() {
 player.addEventListener(Twitch.Player.PLAYING, function() {
 	console.log(player.getQualities());
 });
+
+var e = document.querySelector('.volume-slider-con');
+var eInner = document.querySelector('.volume-slider');
+var drag = false;
+e.addEventListener('mousedown',function(ev){
+   drag = true;
+   updateBar(ev.clientY);
+});
+document.addEventListener('mousemove',function(ev){
+   if(drag){
+      updateBar(ev.clientY);
+   }
+});
+document.addEventListener('mouseup',function(ev){
+ drag = false;
+});
+var updateBar = function (x, vol) {
+   var volume = e;
+        var percentage;
+        //if only volume have specificed
+        //then direct update volume
+        if (vol) {
+            percentage = vol * 100;
+        } else {
+            var position = x - volume.offsetTop;
+            percentage = 100 * position / volume.clientHeight;
+        }
+
+        if (percentage > 100) {
+            percentage = 100;
+        }
+        if (percentage < 0) {
+            percentage = 0;
+        }
+
+        //update volume bar and video volume
+        eInner.style.height = percentage +'%';
+		document.getElementById('volume-knob').style.top = (percentage - 5) / 2.5 +'px';
+		//eInner.style.top = 100 - percentage + 'px';
+	var newVolume = 1 - percentage / 100
+        player.setVolume(newVolume);
+};
