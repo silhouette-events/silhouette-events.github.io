a72b9ec0e47cb5b80d809ac14798b21cb282d5c9
Isaiah Meek <50851883+Isaiah-DSW@users.noreply.github.com>
2020-04-21 13:01:12 +1200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,16 +1,33 @@
 var volumeSlider = document.getElementById("volume-slider");
 var volumeSliderDisplay = document.getElementById("volume-slider-display");
-volumeSliderDisplay.style.width = volumeSlider.value * 0.35 + 'px';
+volumeSliderDisplay.style.width = volumeSlider.value * 0.94 + 'px';
+var preMuteVolume = 10;
+
+if (volumeSlider.value > 50) {
+	document.getElementById('stream-button-volume-full').style.display = 'inline-block';
+	document.getElementById('stream-button-volume-low').style.display = 'none';
+	document.getElementById('stream-button-volume-mute').style.display = 'none';
+} else {
+	if (volumeSlider.value == 0) {
+		document.getElementById('stream-button-volume-full').style.display = 'none';
+		document.getElementById('stream-button-volume-low').style.display = 'none';
+		document.getElementById('stream-button-volume-mute').style.display = 'inline-block';
+	} else {
+		document.getElementById('stream-button-volume-full').style.display = 'none';
+		document.getElementById('stream-button-volume-low').style.display = 'inline-block';
+		document.getElementById('stream-button-volume-mute').style.display = 'none';
+	}
+}
 
 const options = {
-	width: 640,
-	height: 360,
+	width: '100%',
+	height: '100%',
 	channel: "monstercat",
 	autoplay: false,
-	controls: true,
+	controls: false,
 };
 
-const player = new Twitch.Player("player", options);
+const player = new Twitch.Player("livestream", options);
 
 player.addEventListener(Twitch.Player.READY, function() {
 	player.setQuality('160p30');
@@ -19,16 +36,61 @@ player.addEventListener(Twitch.Player.READY, function() {
 });
 
 volumeSlider.oninput = function() {
-	volumeSliderDisplay.style.width = volumeSlider.value * 0.35 + 'px';
+	volumeSliderDisplay.style.width = volumeSlider.value * 0.94 + 'px';
 	player.setVolume(volumeSlider.value / 100);
+	if (volumeSlider.value > 50) {
+		document.getElementById('stream-button-volume-full').style.display = 'inline-block';
+		document.getElementById('stream-button-volume-low').style.display = 'none';
+		document.getElementById('stream-button-volume-mute').style.display = 'none';
+	} else {
+		if (volumeSlider.value == 0) {
+			document.getElementById('stream-button-volume-full').style.display = 'none';
+			document.getElementById('stream-button-volume-low').style.display = 'none';
+			document.getElementById('stream-button-volume-mute').style.display = 'inline-block';
+		} else {
+			document.getElementById('stream-button-volume-full').style.display = 'none';
+			document.getElementById('stream-button-volume-low').style.display = 'inline-block';
+			document.getElementById('stream-button-volume-mute').style.display = 'none';
+		}
+	}
+}
+
+function streamMute() {
+	preMuteVolume = volumeSlider.value;
+	volumeSlider.value = '0';
+	volumeSliderDisplay.style.width = '0px';
+	document.getElementById('stream-button-volume-full').style.display = 'none';
+	document.getElementById('stream-button-volume-low').style.display = 'none';
+	document.getElementById('stream-button-volume-mute').style.display = 'inline-block';
+	player.setMuted(true);
+}
+
+function streamUnmute() {
+	if (preMuteVolume == 0) {
+		preMuteVolume = 25;
+	}
+	volumeSlider.value = preMuteVolume;
+	volumeSliderDisplay.style.width = volumeSlider.value * 0.94 + 'px';
+	document.getElementById('stream-button-volume-mute').style.display = 'none';
+	if (preMuteVolume > 50) {
+		document.getElementById('stream-button-volume-full').style.display = 'inline-block';
+		document.getElementById('stream-button-volume-low').style.display = 'none';
+	} else {
+		document.getElementById('stream-button-volume-full').style.display = 'none';
+		document.getElementById('stream-button-volume-low').style.display = 'inline-block';
+	}
+	player.setMuted(false);
+	player.setVolume(preMuteVolume / 100);
 }
 
 player.addEventListener(Twitch.Player.PLAY, function() {
-	document.getElementById('status').innerHTML = 'playing';
+	document.getElementById('stream-button-pause').style.display = 'inline-block';
+	document.getElementById('stream-button-play').style.display = 'none';
 });
 
 player.addEventListener(Twitch.Player.PAUSE, function() {
-	document.getElementById('status').innerHTML = 'paused';
+	document.getElementById('stream-button-play').style.display = 'inline-block';
+	document.getElementById('stream-button-pause').style.display = 'none';
 });
 
 player.addEventListener(Twitch.Player.ONLINE, function() {
