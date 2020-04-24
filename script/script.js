1cc85ca2b90e63b43db85eaba2af17e0d1402b5f
Isaiah Meek <50851883+Isaiah-DSW@users.noreply.github.com>
2020-04-24 19:49:09 +1200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -44,7 +44,7 @@ if (volumeSlider.value > 50) {
 const options = {
 	width: '100%',
 	height: '100%',
-	channel: "beatsturningstaff",
+	channel: "iwicollective",
 	autoplay: true,
 	controls: false,
 };
